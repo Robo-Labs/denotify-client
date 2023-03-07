354de1a0ad2ff891784d7ef7d85973d6ce9afeaa
SmoothBot <smoothbot123@gmail.com>
2023-03-07 23:39:27 +1100

    fixing bad imports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { DeNotifyClient } from "./denotifyclient.js";
 import { AlertBuilder } from "./alertbuilder.js";
 import { FunctionBuilder } from "./functionbuilder.js"
 import { FilterBuilder } from "./util/filter.js"
-import * as Trigger from "./triggers"
-import * as Notification from "./notifications"
+import * as Trigger from "./triggers/index.js"
+import * as Notification from "./notifications/index.js"
 
 export { 
 	DeNotifyClient,
