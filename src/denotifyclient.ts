34bd2da4657250f499aa09cc399b00fbbc07880f
SmoothBot <smoothbot123@gmail.com>
2023-04-17 14:51:31 +1000

    fix: path import bug

--- src/denotifyclient.ts
+++ src/denotifyclient.ts
@@ -19,7 +19,6 @@ import {
 	TriggerTypeRawId
 } from './triggers/trigger.js'
 import * as yup from 'yup'
-import path from 'path'
 
 const toFunctionsUrl = (id: string) => {
 	return `https://${id}.functions.supabase.co/`
@@ -224,10 +223,10 @@ export class DeNotifyClient {
 
 	private async request(
 		method: 'get' | 'post' | 'patch' | 'delete',
-		endpoint: string,
+		pathname: string,
 		options: { body?: any; params?: any } = {}
 	) {
-		const url = new URL(path.join(this.url, endpoint))
+		const url = new URL(pathname, this.url)
 
 		// append params
 		if (options.params) {
